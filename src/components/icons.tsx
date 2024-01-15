5841429f9a2ab5978bf841644d5cd29d00b14115
Racheal Muchiri <rachealloveo6@gmail.com>
2024-01-15 13:52:34 +0300

    Update icons.tsx

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,7 +1,7 @@
 import { LucideProps } from "lucide-react";
 
 export const Icons = {   
-  logo: (props: LucideProps) => (
+  logo: (props: LucideProps) => (         
     <svg {...props} viewBox="0 0 512 512">
       <g>
         <path
